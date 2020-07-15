6674c9099d07ab588b733cb82399c6b374626928
vasily-esipenko <esipenkovasilij@gmail.com>
2020-07-15 21:48:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ var app = new Vue({
             {id: 1, task: "Wake up", completed: false},
             {id: 2, task: "Have a breakfast", completed: false},
             {id: 3, task: "Write some code", completed: false}
-        ]
+        ],
+        zeroTasks: "You don't have any tasks =(\n Try to add something!"
     },
     methods: {
         add: function() {
-            const newTask = {id: 1, task: this.taskItem, completed: false}
+            const newTask = {id: this.tasks.length+1, task: this.taskItem, completed: false}
             this.tasks.push(newTask);
+        },
+        complete: function(id) {
+            this.tasks[id].completed = !this.tasks[id].completed;
+            console.log(this.tasks[id].completed);
+        },
+        delete: function(id) {
+            this.tasks.splice(id, 1);
+            console.log(this.tasks.task);
         }
     }
 });
