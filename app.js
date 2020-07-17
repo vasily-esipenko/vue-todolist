cc46a16ce6349b59dbe7e445bb8b0df967e3501d
vasily-esipenko <esipenkovasilij@gmail.com>
2020-07-17 13:17:25 +0300

    Update app.js, add localStorage

--- app.js
+++ app.js
@@ -2,19 +2,16 @@ var app = new Vue({
     el: "#app",
     data: {
         taskItem: "",
-        tasks: [
-            {task: "Wake up", completed: false},
-            {task: "Have a breakfast", completed: false},
-            {task: "Write some code", completed: false}
-        ],
+        tasks: this.tasks = JSON.parse(localStorage.getItem("items")),
         zeroTasksOne: "You don't have any tasks now😌",
-        zeroTasksTwo: "Try to add one☝️"
+        zeroTasksTwo: "Try to add one☝️",
     },
     methods: {
         add() {
             if (this.taskItem.length > 0) {
                 const newTask = {task: this.taskItem, completed: false}
                 this.tasks.push(newTask);
+                localStorage.setItem("items", JSON.stringify(this.tasks));
                 this.taskItem = "";
             } else {
                 alert("You didn't add any tasks");
@@ -23,9 +20,11 @@ var app = new Vue({
         },
         complete(id) {
             this.tasks[id].completed = !this.tasks[id].completed;
+            localStorage.setItem("items", JSON.stringify(this.tasks));
         },
         remove(id) {
             this.tasks.splice(id, 1);
+            localStorage.setItem("items", JSON.stringify(this.tasks))
         }
     }
 });
